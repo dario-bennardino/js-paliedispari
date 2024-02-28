3ee54e9c6902dfb6834842752ce339de5a606893
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-02-28 15:35:52 +0100

    [add] esercizio parola palidroma completo

--- Js/script.js
+++ Js/script.js
@@ -1,2 +1,17 @@
 let parola = prompt("Inserisci una parola");
 
+//Richiamo la funzione per controllare se la parola inserita è palidroma ed aggiungo allert
+
+if (parolaPalidroma(parola)){
+    alert("la parola inserita è palidroma");
+} else{
+    alert("La parola inserita non è palidroma");
+}
+
+
+//Definisco la funzione per il controllo della parola palidroma
+
+function parolaPalidroma(parola){
+    // Confronta la parola inserita al suo inverso
+    return parola === parola.split("").reverse().join("");
+}
